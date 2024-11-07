07c34ebc74df2c83274614a3ece08f3daf0a76ea
Muthimaeenah <muthimaeenah@gmail.com>
2024-11-07 11:00:01 +0100

    image sizes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 1500, // Set limit to 1500 KB
+  },
   plugins: [react()],
 })
